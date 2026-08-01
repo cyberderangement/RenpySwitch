195df25b93058afe1518052c6be4642646dc6ce7
cyberderangement <cyberderangement@users.noreply.github.com>
2026-08-01 14:18:21 +0300

    python2 fix #4

--- setup.bash
+++ setup.bash
@@ -36,6 +36,7 @@ pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 
 curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
 curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/python27-switch.zip
 curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+dkp-pacman -Rdd --noconfirm dkp-meson-scripts || true
 dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
 dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 unzip -qq python27-switch.zip -d $DEVKITPRO/portlibs/switch
